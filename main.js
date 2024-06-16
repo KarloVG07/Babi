a99d73a75ef4a8531076afa8c48e218b910104ab
KarloVG07 <garciakarlo23@yahoo.com>
2024-06-16 17:30:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,36 +5,36 @@ const btnYes = document.querySelector(".btn-yes");
 const btnNo = document.querySelector(".btn-no");
 
 function getRandomNumber(min, max) {
-  // Calculate the random number between min and max (inclusive)
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Calculate the random number between min and max (inclusive)
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  return randomNumber;
+    return randomNumber;
 }
 
-btnNo.addEventListener("mouseover", (event) => {
-  const containerHeight = container.getBoundingClientRect().height;
-  const containerWidth = container.getBoundingClientRect().width;
-  const btnHeight = btnNo.getBoundingClientRect().height;
-  const btnWidth = btnNo.getBoundingClientRect().width;
-  const btnTop = btnNo.getBoundingClientRect().top;
-  const btnLeft = btnNo.getBoundingClientRect().left;
+btnNo.addEventListener("click", (event) => {
+    const containerHeight = container.getBoundingClientRect().height;
+    const containerWidth = container.getBoundingClientRect().width;
+    const btnHeight = btnNo.getBoundingClientRect().height;
+    const btnWidth = btnNo.getBoundingClientRect().width;
+    const btnTop = btnNo.getBoundingClientRect().top;
+    const btnLeft = btnNo.getBoundingClientRect().left;
 
-  let newTop = btnTop;
-  let newLeft = btnLeft;
-  while (Math.abs(newTop - btnTop) < containerHeight / 3) {
-    newTop = getRandomNumber(0, containerHeight - btnHeight);
-  }
+    let newTop = btnTop;
+    let newLeft = btnLeft;
+    while (Math.abs(newTop - btnTop) < containerHeight / 3) {
+        newTop = getRandomNumber(0, containerHeight - btnHeight);
+    }
 
-  while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
-    newLeft = getRandomNumber(0, containerWidth - btnWidth);
-  }
+    while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
+        newLeft = getRandomNumber(0, containerWidth - btnWidth);
+    }
 
-  btnNo.style.top = Math.floor(newTop) + "px";
-  btnNo.style.left = Math.floor(newLeft) + "px";
+    btnNo.style.top = Math.floor(newTop) + "px";
+    btnNo.style.left = Math.floor(newLeft) + "px";
 });
 
 btnYes.addEventListener("click", (e) => {
-  btnNo.classList.add("hide");
-  imageOne.classList.add("hide");
-  imageTwo.classList.remove("hide");
+    btnNo.classList.add("hide");
+    imageOne.classList.add("hide");
+    imageTwo.classList.remove("hide");
 });
